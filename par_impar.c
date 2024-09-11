8d8f58c3ce89bac4d103802bb88ff554bb804f67
Bryan Lucas <lbryanz235@gmail.com>
2024-09-11 13:16:37 +0000

    Foi feito o treino de fluxo de dois fatores

---
+++ par_impar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(){
+    int num;
+    
+printf ("Digite um número\n");
+     scanf ("%d",&num);
+
+     if(num %2 == 0 ){
+
+printf("o número digitado é par\n");
+}
+
+else{
+
+printf("O número digitado é impar");
+}
+
+return 0;
+
+}
